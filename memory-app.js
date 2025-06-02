114ceeb02b89557202505fcabf8810b729332fed
Elsie <156831523+oakgreybookshelf@users.noreply.github.com>
2025-06-01 22:54:16 -0400

    linking submit buttons to memory pages 3-4
    also checking questions answers

--- memory-app.js
+++ memory-app.js
@@ -2,3 +2,23 @@ function nextPage() {
             // Replace with your next page URL 
             window.location.href = 'memory2.html'; 
         } 
+
+function next() { 
+            document.getElementById('submit-btn').addEventListener('click', function() {
+                const selectedAnswer = document.querySelector('input[name="colors"]:checked');
+                if (!selectedAnswer) {
+                    alert("Please select an answer.");
+                    return;
+                }
+                if (selectedAnswer.value === "yellow") {
+                    window.location.href = 'memory3.html'; 
+                } else {
+                    window.location.href = 'memory4.html';
+                }
+            }); 
+        } 
+
+function restart() { 
+            // Replace with your next page URL 
+            window.location.href = 'memory.html'; 
+        } 
