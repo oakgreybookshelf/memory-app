0156e62abfa91943ac856f8bf690089a9637b40d
Elsie <156831523+oakgreybookshelf@users.noreply.github.com>
2025-07-17 12:27:36 -0500

    Update memory-app.js

--- memory-app.js
+++ memory-app.js
@@ -4,6 +4,7 @@ function nextPage() {
         } 
 
 function next() { 
+            document.getElementById('submit-btn').addEventListener('click', function() {
                 const selectedAnswer = document.querySelector('input[name="colors"]:checked');
                 if (!selectedAnswer) {
                     alert("Please select an answer.");
